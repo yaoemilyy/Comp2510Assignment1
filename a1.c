a32188b310e016521fb3e53ce73a714b6cd2d3da
Emily Yao <emilyyao9898_@hotmail.com>
2023-10-11 05:09:17 -0700

    insert space and justifyline function

--- a1.c
+++ a1.c
@@ -2,33 +2,101 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+void insertSpaces(char *line, int line_len, int total_len) {
+    int word_count = 0;
+    char *word = line;
+    while (*word != '\0') {
+        if (*word == ' ') {
+            word_count++;
+            word++;
+        } else {
+            word++;
+        }
+    }
+
+    if (word_count == 0) {
+        // Center a single word
+        int total_spaces = total_len - line_len;
+        int spaces_before = total_spaces / 2;
+        int spaces_after = total_spaces - spaces_before;
+
+        for (int i = 0; i < spaces_before; i++) {
+            printf(" ");
+        }
+        printf("%s", line);
+        for (int i = 0; i < spaces_after; i++) {
+            printf(" ");
+        }
+        printf("\n");
+        return;
+    }
+
+    int spaces_to_insert = total_len - line_len + word_count - 1;
+    int spaces_before_word = spaces_to_insert / word_count;
+    int extra_spaces = spaces_to_insert % word_count;
+
+    int word_started = 0;
+    for (int i = 0; i < strlen(line); i++) {
+        if (line[i] != ' ') {
+            if (word_started) {
+                if (extra_spaces > 0) {
+                    printf(" ");
+                    extra_spaces--;
+                }
+                for (int j = 0; j < spaces_before_word; j++) {
+                    printf(" ");
+                }
+            }
+            word_started = 1;
+            printf("%c", line[i]);
+        } else {
+            printf(" ");
+            word_started = 0;
+        }
+    }
+
+    printf("\n");
+}
+
+
+
 void justifyLine(const char *line, int lineLength) {
-   
     int len = strlen(line);
 
-    if (len > lineLength) { 
-        printError("Error. The word `processor can't display the output.");
+    if (len > lineLength) {
+        fprintf(stderr, "Error. The word processor can't display the output.\n");
     } else if (len == lineLength) {
         printf("%s\n", line);
     } else {
-        // check how much spaces need to be added 
+        insertSpaces(line, len, lineLength);
     }
-
 }
 
+
+
+
 int main(int argc, char *argv[]) {
-    // Check for the correct number of command-line arguments
+
+   // Check for the correct number of command-line arguments
     if (argc != 3) {
-        printError("Usage: <executable> <lineLength> <inputFile>");
+        fprintf(stderr, "Usage: %s <lineLength> <inputFile>\n", argv[0]);
+        return 1;
     }
 
-    int lineLength = atoi(argv[2]);
-    char *inputFileName = argv[3];
+    int lineLength = atoi(argv[1]);
+    if (lineLength <= 0) {
+        fprintf(stderr, "Error. The word processor can't display the output.\n");
+        return 1;
+    }
+
+    char *inputFileName = argv[2];
 
     // Open the input file
     FILE *inputFile = fopen(inputFileName, "r");
     if (inputFile == NULL) {
-        printError("Error opening input file");
+        fprintf(stderr, "Error opening input file\n");
+        return 1;
     }
 
     char line[1024]; // Might Change
